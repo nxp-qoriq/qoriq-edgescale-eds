a71c083d255d7d71a9b5eec75f8b3133171caad3
Jiwei Fu <Jiwei.Fu@nxp.com>
2017-12-20 16:53:34 +0800

    Add startup script to push device public ipaddress to cloud
    update env.sh to remove hostname checking
    
    Signed-off-by: Jiwei Fu <Jiwei.Fu@nxp.com>

--- startup/env.sh
+++ startup/env.sh
@@ -6,21 +6,6 @@
 
 #!/bin/bash
 
-hostnametail=ls1043.00A83.mgw.nxp
-hostnameid=`date |md5sum|cut -c 1-5`
-
-#hostnameid=00056
-
-hostname=$(hostname)
-if [[ "$hostname" == "localhost" || "$hostname" == "nxp-ls1043-00035" ]]; then
-	echo $hostnameid.$hostnametail > /etc/hostname 
-	hostname -F /etc/hostname
-	#sed -i /nxp/d /etc/hosts
-	echo "127.0.0.1       $hostnameid.$hostnametail" >>/etc/hosts
-	echo "new hostname $hostnameid.$hostnametail"
-fi
-
-echo "hostname $(hostname)"
 testserver="www.baidu.com"
 
 curl --connect-timeout 10  $testserver >/dev/null 2>&1
